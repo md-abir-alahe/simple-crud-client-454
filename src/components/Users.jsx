bd0c7076504dd4a163b803134c2f92846b4a7047
md-abir-alahe <mdabiralahe@gmail.com>
2023-05-09 00:20:56 +0600

    delete a user from the database using ui click button

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -6,12 +6,31 @@ const Users = () => {
 
     const users = useLoaderData();
 
+    const handleDelete = _id => {
+        console.log('delete',_id);
+        fetch(`http://localhost:5000/users/${_id}`,{
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if (data.deletedCount>0){
+                alert('deleted successfully')
+            }
+        })
+    }
+
     return (
         <div>
             <h2>user : {users.length}</h2>
             <div>
                 {
-                    users.map(user => <p key={user._id}>{user.name} : {user.email}</p>)
+                    users.map(user => <p key={user._id}>
+                     {user.name} : {user.email}
+                        <button 
+                            onClick={()=>handleDelete(user._id)}
+                        >X</button> {user._id}
+                    </p>)
                 }
             </div>
         </div>
