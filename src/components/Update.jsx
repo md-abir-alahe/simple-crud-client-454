db5639f8e82d0e218a875960daa030038c21d32b
md-abir-alahe <mdabiralahe@gmail.com>
2023-05-09 16:50:41 +0600

    put and patch and update user to database

---
+++ src/components/Update.jsx
@@ -0,0 +1,42 @@
+import { useLoaderData } from 'react-router-dom';
+
+const Update = () => {
+    const loadedUser = useLoaderData();
+
+    const handleUpdate = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const updatedUser = {name, email}
+        console.log(name, email);
+        fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type':'application/json'
+            },
+            body: JSON.stringify(updatedUser)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.modifiedCount>0){
+                alert('user updated successful')
+            }
+        })
+    }
+    return (
+        <div>
+            <h3>Update information of {loadedUser.name}</h3>
+            <form onSubmit={handleUpdate}>
+                <input type="text" defaultValue={loadedUser?.name} name="name" id="" />
+                <br />
+                <input type="email" defaultValue={loadedUser?.email} name="email" id="" />
+                <br />
+                <input type="submit" value="Update" />
+            </form>
+        </div>
+    );
+};
+
+export default Update;
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Users = () => {
 
@@ -30,6 +30,9 @@ const Users = () => {
                 {
                     users.map(user => <p key={user._id}>
                      {user.name} : {user.email}
+                        <Link to={`/update/${user._id}`}>
+                            <button>Update</button>
+                        </Link>
                         <button 
                             onClick={()=>handleDelete(user._id)}
                         >X</button> {user._id}
